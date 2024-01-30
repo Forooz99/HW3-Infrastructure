742ce28c3ce567c179fdf69bc04c3a296d2224cf
hanna <hanna.chegini@gmail.com>
2024-01-30 23:28:16 +0330

    Add Locust test and dockerfile

--- src/locust/locustfile.py
+++ src/locust/locustfile.py
@@ -1,3 +1,12 @@
 from locust import HttpUser, between, task
+import random
 
-# TODO PART 5
+class myUser(HttpUser):
+    wait_time = between(1, 3)
+    host = "http://localhost"
+
+    @task
+    def myTask(self):
+        n = random.randint(1, 10)
+        k = random.randint(1, 10)
+        self.client.get("/api/", params={"n": n, "k": k})
